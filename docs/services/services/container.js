3f29051418949039622d40ffb21ced7977586bb5
Lee Kolitz <leekolitz@MacBookPro.ku>
2025-04-28 13:05:55 +0300

    “non”

--- docs/services/services/container.js
+++
@@ -1,7 +0,0 @@
-// src/services/container.js
-export class ServiceContainer {
-  constructor() { this.services = new Map(); }
-  register(name, svc) { this.services.set(name, svc); }
-  get(name) { return this.services.get(name); }
-}
-export const serviceContainer = new ServiceContainer();
